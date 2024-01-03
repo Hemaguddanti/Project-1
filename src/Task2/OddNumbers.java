52df266c7f8175f4a03f2f269780b11120906889
u <u@DESKTOP-8P91KV8>
2024-01-03 15:17:38 +0530

    Nov Team

---
+++ src/Task2/OddNumbers.java
@@ -0,0 +1,23 @@
+package Task2;
+
+public class OddNumbers {
+	
+public static void main(String[] args) {
+			
+			int limit = 10; // Change this value to set the limit of odd numbers to generate
+			
+			System.out.println("Odd numbers:");
+			
+			for (int i = 1; i <= limit; i++) {
+				
+			if(i % 2 != 0) {
+				
+				System.out.println(i);
+					
+				}
+			}
+		}
+
+	}
+
+
